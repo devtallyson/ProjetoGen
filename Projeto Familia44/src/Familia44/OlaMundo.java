42f82ee134183909764ce2de179213d52de29e9e
Tallyson Aureliano <contato.tallyson@live.com>
2022-01-26 15:37:43 -0300

    Add exercicio26_01

---
+++ Projeto Familia44/src/Familia44/OlaMundo.java
@@ -0,0 +1,33 @@
+package Familia44;
+
+import java.util.Scanner;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double nota1,nota2,nota3,media;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("\nEntre com a nota1 :");
+		nota1 = leia.nextFloat();
+		System.out.println("\nEntre com a nota2 :");
+		nota2 = leia.nextFloat();
+		System.out.println("\nEntre com a nota3 :");
+		nota3 = leia.nextFloat();
+		
+		media = (nota1+nota2+nota3) / 3;
+		
+		System.out.println("\nMédia aritmétca:"+media);
+		System.out.printf("\nMédia aritmética: %2.2f e a nota1: %2.2f",media,nota1);
+		
+		nota1 = Math.sqrt(nota2);
+		nota2 = Math.pow(nota3, 3);
+		nota1 = nota2 % nota3;
+		
+		
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,20 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args){
+		
+		double salario = 1250.70;
+		
+		System.out.println("salario");
+		
+		System.out.println("meu salario é " + salario);
+		
+		int divisao = 5 / 2;
+		System.out.println(divisao);
+		
+		
+		
+	}
+	
+	
+}
